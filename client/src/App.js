e94ffeece6580806d6ab79411a6bc4af905e8620
Potcharapol Nokyoo <64063112+inkxk@users.noreply.github.com>
2023-01-10 22:22:41 +0700

    completed get access_token

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import Login from "./Login";
+import Dashboard from "./Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const code = new URLSearchParams(window.location.search).get("code");
+
 function App() {
-    return <Login />;
+    return code ? <Dashboard code={code}/> : <Login />;
 }
 
 export default App;
+
+// login ผ่าน spotify-api-official จะได้ user-code อยู่ใน url
+// get user-code จาก url ถ้ามี code แล้วจะส่งไปหน้า dashboard 
+// นำ user-code ไป fetch data จาก api server หลังบ้าน
+// server หลังบ้านใช้ lib spotify-web-api-node ในการจัดการแทน spotify-api-official
